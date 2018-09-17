08e1e858817fc9841d8f8af200c69cd015d7238b
Daniel Cater <dcater1@my.westga.edu>
2018-09-17 16:48:28 -0400

    Added the Feedback Class

---
+++ CaterProject01/CaterProject01/Model/Feedback.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaterProject01.Model
+{
+    public class Feedback
+    {
+        private int clientID;
+        private int incidentID;
+        private int serviceTime;
+        private int efficiency;
+        private int resolution;
+        private string comments;
+        private bool contact;
+        private string contactMethod;
+
+        public int ClientID
+        {
+            get => this.clientID;
+            set => this.clientID = value;
+        }
+
+        public int IncidentID
+        {
+            get => this.incidentID;
+            set => this.incidentID = value;
+        }
+
+        public int ServiceTime
+        {
+            get => this.serviceTime;
+            set => this.serviceTime = value;
+        }
+
+        public int Efficiency
+        {
+            get => this.efficiency;
+            set => this.efficiency = value;
+        }
+
+        public int Resolution
+        {
+            get => this.resolution;
+            set => this.resolution = value;
+        }
+
+        public string Comments
+        {
+            get => this.comments;
+            set => this.comments = value;
+        }
+
+        public bool Contact
+        {
+            get => this.contact;
+            set => this.contact = value;
+        }
+
+        public string ContactMethod
+        {
+            get => this.contactMethod;
+            set => this.contactMethod = value;
+        }
+    }
+}
